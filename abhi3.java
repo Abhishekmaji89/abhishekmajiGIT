058f97f6a63d2399d70e8046f2d3f478c7ade4bc
Abhishek <abhishekmaji71@gmail.com>
2020-02-01 19:28:21 +0530

    adding 2 more files in the master

---
+++ abhi3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class A
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("I am a student of university of engineering and management kolkata");
+		
+	}	
+}
+		
