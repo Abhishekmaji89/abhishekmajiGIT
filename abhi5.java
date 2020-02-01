09cc16451cc1ed93cbc52cb4d6cd4d7eb7b84923
Abhishek <abhishekmaji71@gmail.com>
2020-02-01 19:41:59 +0530

    lastet version of abhi5.java

--- abhi5.java
+++ abhi5.java
@@ -5,7 +5,7 @@ class A
 	public static void main(String[] args)
 	{
 		
-		System.out.println("My love playing computer games");
+		System.out.println("My love playing computer games and reading comic books");
 		
 	}	
 }
